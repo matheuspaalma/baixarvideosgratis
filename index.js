397be43fd9f8595d630b5af083f6c341a0f0a3a2
Matheus Palma <matheuspaalma@gmail.com>
2021-05-14 20:42:40 -0300

    vercel upload 20

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var path = require('path');
 const app = express();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(4000, () => {
 
